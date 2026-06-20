d571b067872d6328b26fe2a5add2b0ccd65888b8
Henrik Ormåsen <henrik.ormasen@gmail.com>
2026-06-20 21:59:54 +0200

    updated and fixed nixos upgrade

--- configuration.nix
+++ configuration.nix
@@ -34,6 +34,9 @@ in
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
+  # Systemd-initrd: nødvendig for root=fstab med LUKS-kryptering på nyere NixOS.
+  boot.initrd.systemd.enable = true;
+
   networking.hostName = "nixos"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
